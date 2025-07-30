b0fd68f5fcbdfbfec784d95247d7c1e4b6e9010e
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-07-30 15:03:27 +0300

    added Eleanor of Aquitaine. added comments to make my life much easier down the line

--- characters.pl
+++ characters.pl
@@ -4,29 +4,37 @@
 
 
 % House de Normandie
-gender(rollo, male).
 
+gender(rollo, male).        % scandinavian adventurer
+
+% son of Rollo and Poppa
 gender(william_longsword, male).
 
+% son of William longsword and Sprota
 gender(richard_the_fearless, male).
 
+% sons of Gunner and Richard the Fearless
 gender(richard_the_good, male).
 gender(robert_archeveque_de_rouen, male).
 gender(mauger_de_corbeil, male).
 
+% sons of Richard the Good and Judith of Rennes
 gender(william_de_talou, male).
 gender(robert_le_magnifique, male).
 gender(richard_iii_de_normandie, male).
 
+% of Robert the magnificent and Herleve
 gender(william_the_conqueror, male).
 
+% of William the Conqueror and Matilda of Flanders
 gender(william_rufus, male).
 gender(robert_courtheuse, male).
 gender(richard_de_normandie, male).
 gender(henry_beauclerc, male).
 
+% spouses of Matilda, daughter to Henry I Beauclerc
 gender(henry_v_holy_roman_emperor, male).
-gender(geoffrey_de_anjou, male).  % both married to empress maud
+gender(geoffrey_de_anjou, male).
 
 % House de Normandie - Females
 gender(poppa, female).
@@ -35,29 +43,33 @@ gender(sprota, female).
 
 gender(gunner, female).
 
-gender(emma_de_normandie, female).
-gender(judith_de_rennes, female).
+gender(emma_de_normandie, female).      % of Gunner and Richard the Fearless
+gender(judith_de_rennes, female).       
 
 gender(herleva, female).
 
-gender(adelaide_comtesse_de_aumale, female).
+gender(adelaide_comtesse_de_aumale, female).        % of Herleve and Robert the Magnificent
 gender(matilda_de_flandre, female).
 
+% daughters of William the Conqueror
 gender(adela_comtesse_de_blois, female).
 gender(cecilia_abbesse, female).
 gender(constance_duchesse_de_bretagne, female).
 
+% Henry Beauclerc's daughter
 gender(empress_maud, female).
 
 
 % House de Blois 
-gender(stephen_comte_de_blois, male).
+gender(stephen_comte_de_blois, male).       % married Adela, one of William the Conqueror's daughters
 
+% of Stephen and Adela
 gender(william_comte_de_sully, male).
 gender(odo_de_blois, male).
 gender(theobald_ii_comte_de_champagne, male).
-gender(stephen_de_blois, male).
+gender(stephen_de_blois, male).     % became King
 
+% sons of King Stephen and Matilda of Boulogne
 gender(eustace_iv_comte_de_boulogne, male).
 gender(william_i_comte_de_boulogne, male).
 
@@ -66,12 +78,16 @@ gender(matilda_i_comtesse_de_boulogne, female).
 
 
 % House Plantagenet
-gender(henry_plantagenet, male).
+gender(henry_plantagenet, male).        % usurped King Stephen -- founded House Plantagenet/Angevin
 gender(geoffrey_comte_de_nantes, male).
 gender(william_fitzempress, male).      % brothers of King Henry II, sons of empress Maud
 
+% of King Henry II Plantagenet and Eleanor of Aquitaine
 gender(william_ix_comte_de_poitiers, male).
 gender(henry_jeune_roi, male).
 gender(richard_coeur_de_lion, male).
 gender(geoffrey_ii_duc_de_bretagne, male).
 gender(king_john, male).
+
+% House PLantagenet - Females
+gender(alienor_d_aquitaine, female).
