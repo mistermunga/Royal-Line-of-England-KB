f3b113557609a306c7b1b0e50c0d45fdcdd088c7
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-07-30 14:13:04 +0300

    premier commit; added william the conqueror's immediate family and ancestors

---
+++ characters.pl
@@ -0,0 +1,46 @@
+% This is a set of characters from the historical viking Rollo to Edward III of England
+
+% === GENDER DECLARATIONS ===
+
+% House de Normandie
+gender(rollo, male).
+
+gender(william_longsword, male).
+
+gender(richard_the_fearless, male).
+
+gender(richard_the_good, male).
+gender(robert_archeveque_de_rouen, male).
+gender(mauger_de_corbeil, male).
+
+gender(william_de_talou, male).
+gender(robert_le_magnifique, male).
+gender(richard_iii_de_normandie, male).
+
+gender(william_the_conqueror, male).
+
+man(william_rufus).
+man(robert_courtheuse).
+man(richard_de_normandie).
+man(henry_beauclerc).
+
+
+% House de Normandie - Females
+gender(poppa, female).
+
+gender(sprota, female).
+
+gender(gunner, female).
+
+gender(emma_de_normandie, female).
+gender(judith_de_rennes, female).
+
+gender(herleva, female).
+
+gender(adelaide_comtesse_de_aumale, female).
+gender(matilda_de_flandre, female).
+
+gender(adela_comtesse_de_blois, female).
+gender(cecilia_abbesse, female).
+gender(constance_duchesse_de_bretagne, female).
+
