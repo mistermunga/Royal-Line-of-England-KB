34c0e2b0653eae3fa2b869ef117eb32eb0538d14
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-07-30 21:55:19 +0300

    added dynastic information

---
+++ dynasties.pl
@@ -0,0 +1,66 @@
+% load the character information
+:- [characters].
+
+% Members of the House of Normandie
+
+dynasty(normandie, [
+    rollo,
+    william_longsword,
+    richard_the_fearless,
+    richard_the_good,
+    robert_archeveque_de_rouen,
+    mauger_de_corbeil,
+    william_de_talou,
+    robert_le_magnifique,
+    richard_iii_de_normandie,
+    william_the_conqueror,
+    william_rufus,
+    robert_courtheuse,
+    richard_de_normanie,
+    henry_beauclerc,
+    emma_de_normandie,
+    adelaide_comtesse_de_aumale,
+    adela_comtesse_de_blois,
+    cecilia_abbesse,
+    constance_duchesse_de_bretagne,
+    empress_maud]).
+
+dynasty(blois, [
+    stephen_comte_de_blois,
+    william_comte_de_sully,
+    odo_de_blois,
+    theobald_ii_comte_de_champagne,
+    stephen_de_blois,
+    eustace_iv_comte_de_boulogne,
+    william_i_comte_de_boulogne]).
+
+dynasty(plantagenet, [
+    geoffrey_de_anjou,
+    henry_plantagenet,
+    geoffrey_comte_de_nantes,
+    william_fitzempress,
+    william_ix_comte_de_poitiers,
+    henry_jeune_roi,
+    richard_coeur_de_lion,
+    geoffrey_ii_duc_de_bretagne,
+    king_john,
+    henry_iii,
+    richard_king_of_the_romans,
+    matilda_duchesse_de_saxe,
+    eleanor_reine_de_castille,
+    joanne_reine_de_sicile,
+    joanne_reine_de_ecosse,
+    empress_isabella,
+    alienor_de_pembroke]).
+
+dynasty(salian, [henry_v_holy_roman_emperor]).
+
+dynasty(ramnulfids, [alienor_d_aquitaine]).
+
+dynasty(rennes, [judith_de_rennes]).
+
+dynasty(baudouinides, [matilda_de_flandre]).
+
+dynasty(dunkeld, [edith_of_scotland]).
+
+dynasty(taillefer, [isabella_d_angouleme]).
