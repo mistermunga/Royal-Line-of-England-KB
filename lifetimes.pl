dc60c50a5483e214c67655bad403d8e65545703b
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-07-31 13:05:58 +0300

    added birth years and death years

---
+++ lifetimes.pl
@@ -0,0 +1,84 @@
+:-[characters].
+
+% Birth Years and Death years for the characters.
+
+% some dates are not known with 100% certainty
+% in which case circa/2 defines a likely range
+% and circa/1 defines a +-1 range from the stated year
+
+% lifetime/3 --> character, birthyear, deathyear
+
+
+% House de Normandie
+
+lifetime(rollo, circa(835, 870), 933).
+lifetime(poppa, circa(880), unknown).
+
+lifetime(william_longsword, circa(893), 942).
+lifetime(sprota, unknown, unknown).
+
+lifetime(richard_the_fearless, 932, 996).
+lifetime(gunner, circa(950), circa(1031)).
+
+lifetime(emma_de_normandie, circa(984), 1052).
+lifetime(richard_the_good, circa(980), 1026).
+lifetime(robert_archeveque_de_rouen, circa(988), 1037).
+lifetime(mauger_de_corbeil, circa(988), circa(1032)).
+
+lifetime(judith_de_rennes, circa(982), circa(1017)).
+lifetime(richard_iii_de_normandie, circa(997, 1001), 1027).
+lifetime(william_de_talou, circa(1035), 1086).
+lifetime(robert_le_magnifique, 1000, 1035).
+
+lifetime(herleva, circa(1005), circa(1050)).
+lifetime(adelaide_comtesse_de_aumale, circa(1030), circa(1089)).
+lifetime(william_the_conqueror, circa(1028), 1087).
+
+lifetime(matilda_de_flandre, circa(1031), 1083).
+lifetime(william_rufus, circa(1057), 1100).
+lifetime(robert_courtheuse, circa(1051), 1134).
+lifetime(henry_beauclerc, circa(1068), 1135).
+lifetime(adela_comtesse_de_blois, circa(1067), 1137).
+lifetime(cecilia_abbesse, circa(1056), 1126).
+lifetime(constance_duchesse_de_bretagne, circa(1057, 1061), 1090).
+
+lifetime(empress_maud, circa(1102), 1167).
+lifetime(henry_v_holy_roman_emperor, circa(1081, 1086), 1125).
+
+
+% House de Blois 
+
+lifetime(stephen_comte_de_blois, circa(1045), 1102).
+lifetime(william_comte_de_sully, circa(1085), circa(1150)).
+lifetime(odo_de_blois, unknown, unknown).
+lifetime(theobald_ii_comte_de_champagne, 1090, 1152).
+lifetime(stephen_de_blois, circa(1092, 1096), 1154).
+
+lifetime(matilda_i_comtesse_de_boulogne, circa(1105), 1152).
+lifetime(eustace_iv_comte_de_boulogne, circa(1130), 1153).
+lifetime(william_i_comte_de_boulogne, circa(1137), 1159).
+
+
+% House Plantagenet
+
+lifetime(geoffrey_de_anjou, 1113, 1151).
+lifetime(henry_plantagenet, 1133, 1189).
+lifetime(geoffrey_comte_de_nantes, 1134, 1158).
+lifetime(william_fitzempress, 1136, 1164).
+
+lifetime(alienor_d_aquitaine, circa(1124), 1204).
+lifetime(william_ix_comte_de_poitiers, 1153, 1156).
+lifetime(henry_jeune_roi, 1155, 1183).
+lifetime(richard_coeur_de_lion, 1157, 1199).
+lifetime(geoffrey_ii_duc_de_bretagne, 1158, 1186).
+lifetime(king_john, 1166, 1216).
+lifetime(matilda_duchesse_de_saxe, 1156, 1189).
+lifetime(eleanor_reine_de_castille, circa(1161), 1214).
+lifetime(joanne_reine_de_sicile, 1165, 1199).
+
+lifetime(isabella_d_angouleme, circa(1187), 1246).
+lifetime(henry_iii, 1216, 1272).
+lifetime(richard_king_of_the_romans, 1209, 1272).
+lifetime(joanne_reine_d_ecosse, 1210, 1238).
+lifetime(empress_isabella, 1214, 1241).
+lifetime(alienor_de_pembroke, 1215, 1275).
